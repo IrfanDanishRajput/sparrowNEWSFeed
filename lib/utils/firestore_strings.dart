9f0ede9700ed704593967aa0642fa85190f8125c
Irfan Danish <irfan.danish@txlabz.com>
2022-01-23 14:33:36 +0500

    Created class that will hold all the firestore related strings

---
+++ lib/utils/firestore_strings.dart
@@ -0,0 +1,16 @@
+class FirebaseStrings {
+  //! collection news feed
+  static const collectionNewsFeed = 'newsFeed';
+  static const headline = 'headline';
+  static const image = 'image';
+  static const summary = 'summary';
+  static const comments = 'comments';
+
+  //! comments collection
+  static const collectionComments = 'comments';
+  static const comment = 'comment';
+  static const newsFeedID = 'newsFeedID';
+
+  //! shared strings
+  static const documentID = 'documentID';
+}
