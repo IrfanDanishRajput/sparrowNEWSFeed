60881cb95dafe61b02adabf326f9eed26dfb48b1
Irfan Danish <irfan.danish@txlabz.com>
2022-01-23 12:30:28 +0500

    Firebase configured

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,19 @@ import 'package:sparrow_news_app/utils/app_strings.dart';
 import 'package:sparrow_news_app/utils/custom_router.dart';
 import 'package:sparrow_news_app/utils/routes.dart';
 
+FirebaseOptions get firebaseOptions => const FirebaseOptions(
+      appId: '1:756941742705:android:7ce841006e2dd1100b71d0',
+      apiKey: "AIzaSyBxFIQoWvuTQcJjn4uKJt-s4_cpWTFHFME",
+      projectId: 'sparrownewsapp',
+      messagingSenderId: '448618578101',
+    );
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+    options: firebaseOptions,
+  );
   runApp(const MyApp());
 }
 
@@ -16,12 +26,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: AppStrings.appName,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Container(),
-        initialRoute: AppRoutes.loginRoute,
-        onGenerateRoute: CustomRouter.allRoutes);
+      debugShowCheckedModeBanner: false,
+      title: AppStrings.appName,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: AppRoutes.loginRoute,
+      onGenerateRoute: CustomRouter.allRoutes,
+    );
   }
 }
